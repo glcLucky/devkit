0060d8abea48987009858cdac4270ffcc31ef4bc
jasper gui <jasper@jasperdeMacBook-Pro.local>
2020-08-16 14:33:25 +0800

    init commit

--- devkit/api.py
+++ devkit/api.py
@@ -11,7 +11,8 @@ api.py
 from . src.utils.viz import mgc_scatter
 from . src.utils.viz import tsne_reducer
 from . src.utils.viz import umap_reducer
-from . src.utils.viz import data_viz
+
+from . src.utils.viz import plot_feature_importances
 
 # confusion matrix
 from . src.utils.model import make_confusion_matrix
